cfacdf80aa4ef4e536a3d19d04d2dd2d490e93e3
Andrey Georgievskiy <73390983+g-aa@users.noreply.github.com>
2022-08-29 21:41:15 +0300

    МТ-0002: Добавление интерфейсов сущностей

---
+++ Mt.Entities.Abstractions/Interfaces/IEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Mt.Entities.Abstractions.Interfaces
+{
+    /// <summary>
+    /// Сущность МТ.
+    /// </summary>
+    public interface IEntity
+    {
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        Guid Id { get; set; }
+    }
+}
